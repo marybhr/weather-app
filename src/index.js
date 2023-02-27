624ee51a7cac0a5c9d7becf42771a2c53039cb8a
marybhr <marzy.bahrami@gmail.com>
2023-02-27 21:26:38 +0330

    added unit conversion feature

--- src/index.js
+++ src/index.js
@@ -1,75 +1,3 @@
-// function toFarenheit(event) {
-//   event.preventDefault();
-//   let farenDeg = document.querySelector("h2");
-//   farenDeg.innerHTML = `39°F`;
-// }
-// let faren = document.querySelector("#far");
-// faren.addEventListener("click", toFarenheit);
-
-// function toCeli(event) {
-//   event.preventDefault();
-//   let celiDeg = document.querySelector("h2");
-//   celiDeg.innerHTML = `4°C`;
-// }
-// let celic = document.querySelector("#celi");
-// celic.addEventListener("click", toCeli);
-
-// temprature Function
-function showTemprature(response) {
-  let celiTemp = response.data.main.temp;
-  let celiTemprature = Math.round(celiTemp);
-  let h1 = document.querySelector("h1");
-  let city = response.data.name;
-  let h2 = document.querySelector("#degree");
-  let feel = document.querySelector("#feel");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#dateElement");
-  let skyElement = document.querySelector("#sky-icon");
-  h1.innerHTML = city;
-  h2.innerHTML = `${celiTemprature}°C`;
-  feel.innerHTML = response.data.weather[0].main;
-  windElement.innerHTML = `Wind speed:${response.data.wind.speed} Km/h`;
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  skyElement.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  skyElement.setAttribute("alt", response.data.weather[0].description);
-}
-let apiKey = "b35c686ba9565ba0ab254c2230937552";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&lang=en&appid=${apiKey}`;
-axios.get(apiUrl).then(showTemprature);
-
-// location display api call
-function showCity(event) {
-  event.preventDefault();
-  let display = document.querySelector("h1");
-  let cityInput = document.querySelector("#inpt-srch");
-  display.innerHTML = `${cityInput.value}`;
-  let apiKey = "b35c686ba9565ba0ab254c2230937552";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&lang=en&appid=${apiKey}`;
-  axios.get(apiUrl).then(showTemprature);
-}
-let place = document.querySelector("#srch-frm");
-place.addEventListener("submit", showCity);
-
-// current location api call
-function searchLocation(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let lat = latitude.toFixed(2);
-  let lon = longitude.toFixed(2);
-  let apiKey = "b35c686ba9565ba0ab254c2230937552";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemprature);
-}
-function userPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-let current = document.querySelector("button");
-current.addEventListener("click", userPosition);
-
 // js for Date
 function formatDate(timestamp) {
   let now = new Date(timestamp);
@@ -93,3 +21,85 @@ function formatDate(timestamp) {
   let day = days[now.getDay()];
   return `${day},${hour}:${minute}`;
 }
+
+// temprature Function
+function showTemprature(response) {
+  let h1 = document.querySelector("h1");
+  city = response.data.name;
+  let degree = document.querySelector("#degree");
+  let feel = document.querySelector("#feel");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#dateElement");
+  let skyElement = document.querySelector("#sky-icon");
+
+  celiTemp = response.data.main.temp;
+  h1.innerHTML = city;
+  degree.innerHTML = Math.round(celiTemp);
+  feel.innerHTML = response.data.weather[0].main;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  skyElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  skyElement.setAttribute("alt", response.data.weather[0].description);
+}
+let apiKey = "b35c686ba9565ba0ab254c2230937552";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&lang=en&appid=${apiKey}`;
+axios.get(apiUrl).then(showTemprature);
+
+// location display api call
+function showCity(event) {
+  event.preventDefault();
+  let display = document.querySelector("h1");
+  let cityInput = document.querySelector("#inpt-srch");
+  display.innerHTML = `${cityInput.value}`;
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&lang=en&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemprature);
+}
+
+// current location api call
+function searchLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let lat = latitude.toFixed(2);
+  let lon = longitude.toFixed(2);
+  let apiKey = "b35c686ba9565ba0ab254c2230937552";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemprature);
+}
+
+function userPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let current = document.querySelector("button");
+current.addEventListener("click", userPosition);
+
+let place = document.querySelector("#srch-frm");
+place.addEventListener("submit", showCity);
+
+function toFarenheit(event) {
+  event.preventDefault();
+  let degree = document.querySelector("#degree");
+  celi.classList.remove("active");
+  far.classList.add("active");
+  let fahrenheiTemperature = (celiTemp * 9) / 5 + 32;
+  degree.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function toCelsius(event) {
+  event.preventDefault();
+  celi.classList.add("active");
+  let celiDeg = document.querySelector("#degree");
+  degree.innerHTML = Math.round(celiTemp);
+}
+
+let celiTemp = null;
+
+let farenheit = document.querySelector("#far");
+farenheit.addEventListener("click", toFarenheit);
+
+let celsius = document.querySelector("#celi");
+celsius.addEventListener("click", toCelsius);
